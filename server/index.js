e48def0862d40ee733602f394c10859d61ce2415
eduardo <eduvela_94@hotmail.com>
2020-10-27 09:51:19 +0100

    frist commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from './lib/logger.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// midlewares
+import httpLoggerMiddleware from './middleware/logger-middleware.js';
+import jsonResponseMiddleware from './middleware/json-response.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+import musicRouter from './routes/music.js';
+
+const HOST = '127.0.0.1';
+const PORT = 5000;
+export const databaseURI = 'mongodb://localhost/mongoose-intro';
+// const databaseURI = 'mongodb://localhost:2700/mongoose-intro
+
+// Creacion del servidor
+const server = express();
+mongoose.connect(databaseURI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+server.use(cors());
+// El servidor utilizará como deserializador de data bodyparser y deserializara en JSON
+server.use(bodyParser.json());
+// Utiliza un middleware que permite tener descripciones mas especificas en la consola
+server.use(httpLoggerMiddleware);
+// Utiliza un middleware que permite crear headers de respuesta que indiquen que el contenido es JSON
+server.use(jsonResponseMiddleware);
+
+// El router de musica
+server.use(musicRouter);
+
+// Sino no hay rutas definidas envia error al cliente
+server.use(errorHandlerMiddleware);
+
+// Inicializa el servidor
+server.listen(PORT, () =>
+  // utilizando el logger de la libreria winston imprimo en consola que el servidor se ha iniciado
+  logger.info(`server listening ${JSON.stringify({ HOST, PORT })}`),
+);
